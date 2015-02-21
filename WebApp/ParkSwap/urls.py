a8250afe1cbe1a64ed7c993183101a78ff0cea35
automotua <adam.junqiang.li@gmail.com>
2015-02-21 16:47:54 -0500

    added logged-in page

--- WebApp/ParkSwap/urls.py
+++ WebApp/ParkSwap/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'ParkSwap.views.home', name='home'),
+    url(r'^logged-in-home$', 'ParkSwap.views.logged_in', name='loggedin')
 )
--- WebApp/ParkSwap/views.py
+++ WebApp/ParkSwap/views.py
@@ -4,20 +4,23 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.views.defaults import page_not_found
 
-# Decorator to use built-in authentication system                                    
+# Decorator to use built-in authentication system
 from django.contrib.auth.decorators import login_required
 
-# Used to create and manually log in a user                                                                                                                                                                
+# Used to create and manually log in a user
 from django.contrib.auth.models import User
-from django.contrib.auth import login 
+from django.contrib.auth import login
 from django.contrib.auth import authenticate
 
-# Django transaction system so we can use @transaction.atomic                                                                                                                                              
-from django.core.urlresolvers import reverse 
+# Django transaction system so we can use @transaction.atomic
+from django.core.urlresolvers import reverse
 from django.db import transaction
 
 
 
 def home(request):
-    
+
     return render(request,'ParkSwap/home.html',{})
+
+def logged_in(request):
+    return render(request, 'ParkSwap/logged-in-home.html',{})
