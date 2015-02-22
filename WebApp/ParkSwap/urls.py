cf37be8ccf6b6e658c2ab5b79b1cb08cb46ff2b6
emaashir789/em <aashir789@gmail.com>
2015-02-21 20:59:20 -0500

    changed the search page and urls

--- WebApp/ParkSwap/urls.py
+++ WebApp/ParkSwap/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'ParkSwap.views.home', name='home'),
-    url(r'^logged-in-home$', 'ParkSwap.views.logged_in', name='loggedin'),
+    url(r'^search$', 'ParkSwap.views.logged_in', name='search'),
 
     url(r'^wait$', 'ParkSwap.views.wait', name='wait'),
 
